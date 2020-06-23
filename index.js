d0206d4b2422933e32fae98ec5631cf2b3f07e3f
Elizabeth Crane <elizabeth.anne.crane@gmail.com>
2020-06-23 10:44:40 -0400

    task 7 complete

--- index.js
+++ index.js
@@ -172,7 +172,10 @@ function copy(arrayClone, array){
 
 //console.log(copy(originalFlavorsCopy, originalFlavors));
 
-/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
+/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. 
+Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. 
+Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". 
+This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
 Your function should accept: 
 
@@ -187,11 +190,20 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
+function filterByWord(array, flavorKey, newArray){
 
-    /*code here*/
+    newArray = [];
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i].includes(flavorKey)){
+            newArray.push(array[i]);
+        }
+
+    } return newArray;
 
 }
+
+console.log(filterByWord(originalFlavors, "Chocolate"));
 
 
 
